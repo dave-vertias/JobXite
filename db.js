c0085480c6eb86cae3f25b9dd7b59850ddd444c1
David Camp <75981851+dave-vertias@users.noreply.github.com>
2021-10-12 20:50:52 -0400

    Add files via upload

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const Pool = require("pg").Pool;
+//pool is function specified by the node-postgres package to interface with PostgreSQL
 
+//This gives database information
 const pool = new Pool({
     user: "postgres",
-    password: "00000",
+    password: "daveRocks!96",
     host: "localhost",
-    port: 0000,
+    port: 5432,
     database: "jobs"
 });
 
